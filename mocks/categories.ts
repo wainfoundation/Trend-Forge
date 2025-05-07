8e1707211d1303c9cd07f1fdb8b05751d6fe6833
Mrwain Organization <mrwainorganization@gmail.com>
2025-05-08 02:20:03 +0800

    Create categories.ts

---
+++ mocks/categories.ts
@@ -0,0 +1,39 @@
+import { Category } from "@/types/article";
+
+export const categories: Category[] = [
+  {
+    id: "1",
+    name: "World",
+    slug: "world",
+  },
+  {
+    id: "2",
+    name: "Business",
+    slug: "business",
+  },
+  {
+    id: "3",
+    name: "Finance",
+    slug: "finance",
+  },
+  {
+    id: "4",
+    name: "Science",
+    slug: "science",
+  },
+  {
+    id: "5",
+    name: "Culture",
+    slug: "culture",
+  },
+  {
+    id: "6",
+    name: "Opinion",
+    slug: "opinion",
+  },
+  {
+    id: "7",
+    name: "Technology",
+    slug: "technology",
+  }
+];
