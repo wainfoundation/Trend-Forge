a75e372344aac305b2378a1d37d172fc65b343ee
Mrwain Organization <mrwainorganization@gmail.com>
2025-05-08 02:22:58 +0800

    Create colors.ts

---
+++ constants/colors.ts
@@ -0,0 +1,28 @@
+export default {
+  light: {
+    text: "#000",
+    background: "#fff",
+    tint: "#e3120b", // Economist red
+    tabIconDefault: "#ccc",
+    tabIconSelected: "#e3120b",
+    secondary: "#f9fafb",
+    border: "#e5e7eb",
+    card: "#fff",
+    error: "#c81009",
+    success: "#34d399",
+    premium: "#e3120b",
+  },
+  dark: {
+    text: "#fff",
+    background: "#121212",
+    tint: "#e3120b", // Economist red
+    tabIconDefault: "#666",
+    tabIconSelected: "#e3120b",
+    secondary: "#1f1f1f",
+    border: "#333",
+    card: "#1c1c1c",
+    error: "#c81009",
+    success: "#34d399",
+    premium: "#e3120b",
+  },
+};
